03b576530fa956b0057a347fe44c339d6ced5e62
Vipul2202 <105905749+Vipul2202@users.noreply.github.com>
2024-01-11 10:48:00 +0530

    first commit

---
+++ src/redux/actions/productsActions.js
@@ -0,0 +1,50 @@
+import { ActionTypes } from "../constants/action-types";
+
+export const setProducts = (products) => {
+  return {
+    type: ActionTypes.SET_PRODUCTS,
+    payload: products,
+  };
+};
+
+export const selectedProduct = (product) => {
+  return {
+    type: ActionTypes.SELECTED_PRODUCT,
+    payload: product,
+  };
+};
+// export const vip = () => {
+// return{
+// type: ActionTypes.vip,
+// // payload:pay,
+// }
+// }
+export const removeSelectedProduct = () => {
+  return {
+    type: ActionTypes.REMOVE_SELECTED_PRODUCT,
+  };
+};
+
+//  export const upDate = () =>{
+//   return {
+//     type: ActionTypes.upDate,
+//     // payload: pay,
+//   };
+// };
+// export const todo = () =>{
+//   return {
+// type: ActionTypes.Todo,
+//   };
+// };
+
+// export  const remove = () => {
+//   return {
+//     type: ActionTypes.Remove ,
+//   }
+// };
+// // export const datafetch = () = {
+// // //   return {
+// // //     type: ActionTypes.datafetch,
+// // //     payload:pay,
+// // //   }
+// // // }
